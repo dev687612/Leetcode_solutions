248d4b778e30af3f1d43ec8f56b1b6f660abcf81
Dev Singh <dingh6876@gmail.com>
2026-02-16 22:19:19 +0530

    Time: 0 ms (100%), Space: 8.4 MB (15.72%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        
+        //base case
+        if(n == 0)
+        {
+            return 0;
+        }
+        int result = 0;
+
+        for(int i=1;i<=32;i++)
+        {
+            result <<= 1;
+
+            result = (result | (n&1));
+
+            n>>=1;
+        }
+        return result;
+    }
+};
