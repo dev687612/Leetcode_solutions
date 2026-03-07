f7333d6e4ff0bc8ef4e5186144d8c32abd97be85
Dev Singh <dingh6876@gmail.com>
2026-03-07 15:18:14 +0530

    Time: 14 ms (71.2%), Space: 14.4 MB (84.8%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     // tc = O(len)
-    // sc = O(2*n)
+    // sc = O(1)
     int minFlips(string s) {
 
         int n = s.length();
 
-        s += s;
+        // s += s;
 
-        string s1, s2;
-        for (int i = 0; i < 2 * n; i++) {
-            s1 += (i % 2 == 0) ? '0' : '1';
-            s2 += (i % 2 == 0) ? '1' : '0';
-        }
+        // string s1, s2;
+        // for (int i = 0; i < 2 * n; i++) {
+        //     s1 += (i % 2 == 0) ? '0' : '1';
+        //     s2 += (i % 2 == 0) ? '1' : '0';
+        // }
 
         // sliding window
 
@@ -24,20 +24,27 @@ public:
         int j = 0;
 
         while (j < 2 * n) {
-            if (s[j] != s1[j]) {
+
+            char exceptedCharS1 = (j % 2 == 0) ? '1' : '0';
+            char exceptedCharS2 = (j % 2 == 0) ? '0' : '1';
+
+            if (s[j % n] != exceptedCharS1) {
                 flip1++;
             }
-            if (s[j] != s2[j]) {
+            if (s[j % n] != exceptedCharS2) {
                 flip2++;
             }
 
             if (j - i + 1 > n) {
                 // shrink the window
-                if (s[i] != s1[i]) {
+                exceptedCharS1 = (i % 2 == 0) ? '1' : '0';
+                exceptedCharS2 = (i % 2 == 0) ? '0' : '1';
+
+                if (s[i % n] != exceptedCharS1) {
                     flip1--;
                 }
 
-                if (s[i] != s2[i]) {
+                if (s[i % n] != exceptedCharS2) {
                     flip2--;
                 }
                 i++;
